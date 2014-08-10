6625376634fe2f020948cbbd899c2d94fec2b7b4
Miguel Mascorro <miguel@lumenbrite.com>
2014-08-10 01:12:38 -0500

    load config settings from config.json

--- accountinfo/AccountInfoService.cs
+++ accountinfo/AccountInfoService.cs
@@ -4,14 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Management;
+using System.Runtime.Serialization.Json;
+using System.IO;
+using System.Runtime.Serialization;
 
 namespace accountinfo
 {
     class AccountInfoService
     {
+
+        public Config config;
+
+        public AccountInfoService()
+        {
+            this.loadConfig();
+        }
+
         public String getSerial()
         {
-
+           
             String serial = "";
             ManagementClass wmi = new ManagementClass("Win32_Bios");
             foreach (ManagementObject bios in wmi.GetInstances())
@@ -20,5 +31,22 @@ namespace accountinfo
             }
             return serial;
         }
+
+        private void loadConfig()
+        {
+           
+            String configPath = System.IO.Path.Combine(".","config.json");
+            String json = File.ReadAllText(configPath, Encoding.UTF8);
+            MemoryStream r = new MemoryStream(Encoding.UTF8.GetBytes(json));
+            DataContractJsonSerializer jser = new DataContractJsonSerializer(typeof(Config));
+            this.config = (Config)jser.ReadObject((Stream)r);
+        }
+    }
+
+    [DataContract]
+    public class Config
+    {
+        [DataMember]
+        public String url;
     }
 }

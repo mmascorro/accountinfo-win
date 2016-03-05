281a160e7154f46c9f61cd3a4618a538d327d1a2
Miguel Mascorro <futuretuesday@gmail.com>
2016-03-05 07:51:43 -0600

    Initial migration to WPF

---
+++ accountinfo-wpf/AccountInfoService.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Text;
+using System.Management;
+using System.Runtime.Serialization.Json;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Net;
+
+namespace accountinfo
+{
+    class AccountInfoService
+    {
+        public Config config;
+        public String serial;
+        public String remoteName;
+        public String account;
+
+        public AccountInfoService()
+        {
+            loadConfig();
+            getSerial();
+            getRemoteInfo();
+        }
+
+        public void getRemoteInfo()
+        {
+            WebRequest request = WebRequest.Create(config.url + "/computers/info/" + serial);
+            request.Method = "GET";
+
+            WebResponse response = request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream);
+
+            var result = reader.ReadToEnd();
+            stream.Dispose();
+            reader.Dispose();
+
+            if (result == "0")
+            {
+                remoteName = "NOT REGISTERED";
+                account = "";
+            }
+            else
+            {
+                string[] remoteInfo = result.Split(',');
+                if (remoteInfo.Length == 1)
+                {
+                    remoteName = remoteInfo[0];
+                    account = "No Account";
+                }
+                else
+                {
+                    remoteName = remoteInfo[0];
+                    account = remoteInfo[1];
+                }
+            }
+        }
+
+        public void getSerial()
+        {
+           
+            ManagementClass wmi = new ManagementClass("Win32_Bios");
+            foreach (ManagementObject bios in wmi.GetInstances())
+            {
+                serial = bios.Properties["Serialnumber"].Value.ToString().Trim();
+            }
+            
+        }
+
+        public String registerWithServer()
+        {
+            WebRequest request = WebRequest.Create(config.url + "/computers/register");
+            request.Method = "POST";
+            String data = String.Format("computer[serial]={0}&computer[name]={1}", serial, Environment.MachineName);
+            byte[] bytes = Encoding.UTF8.GetBytes(data);
+            request.ContentLength = bytes.Length;
+
+            Stream requestStream = request.GetRequestStream();
+            requestStream.Write(bytes, 0, bytes.Length);
+
+            WebResponse response = request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream);
+
+            var result = reader.ReadToEnd();
+            stream.Dispose();
+            reader.Dispose();
+
+            return result;
+        }
+
+        private void loadConfig()
+        {
+            String configPath = System.IO.Path.Combine(".","config.json");
+            String json = File.ReadAllText(configPath, Encoding.UTF8);
+            MemoryStream r = new MemoryStream(Encoding.UTF8.GetBytes(json));
+            DataContractJsonSerializer jser = new DataContractJsonSerializer(typeof(Config));
+            config = (Config)jser.ReadObject((Stream)r);
+        }
+
+    }
+
+    [DataContract]
+    public class Config
+    {
+        [DataMember]
+        public String url;
+    }
+}
---
+++ accountinfo-wpf/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using accountinfo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace accountinfo_wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private AccountInfoService ais;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            ais.registerWithServer();
+        }
+
+        private void doStuff(object sender, RoutedEventArgs e)
+        {
+            ais = new AccountInfoService();
+
+            serialTextBox.Text = ais.serial;
+
+            localComputerNameLabel.Content = Environment.MachineName;
+
+            ais.getRemoteInfo();
+            remoteComputerNameLabel.Content = ais.remoteName;
+            adobeAccountTextBox.Text = ais.account;
+        }
+    }
+}
